6e9ce130cb59adc74164f90ab1eaff4e06032964
Kival Mahadew <kivalm@protonmail.com>
2022-04-23 19:14:55 +0200

    fin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use yew::prelude::*;
+
+// the function component that will hold all our content
+// Everything within the html! macro will be displayed on our page
+#[function_component(Model)]
+fn model() -> Html {
+    html! {
+        <div>{"Hello World"}</div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Model>();
 }
